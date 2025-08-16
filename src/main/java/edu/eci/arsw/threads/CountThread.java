a7bb891b2663cd07705ee2b1d0cc459f93c696b6
unknown <juan.dgomez@labinfo.is.escuelaing.edu.co>
2025-08-16 09:00:31 -0500

    first commit#

---
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.threads;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class CountThread extends Thread{
+    public CountThread(int inicio, int fin) {
+        for (int i = inicio; i <= fin; i++) {
+            System.out.println(i);
+        }
+    }
+}
